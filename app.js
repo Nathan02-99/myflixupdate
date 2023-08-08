70e553d3e55ffccc5d173569b644b5861af27bb5
Jonathan Gichini <gichinijonathan@gmail.com>
2023-08-08 15:49:17 +0300

    new changes

--- app.js
+++ app.js
@@ -99,16 +99,16 @@ app.get("/api/genre/:genreName", async (req, res) => {
     const genre = await Genre.findOne({ name: genreName });
 
     if (genre) {
-      res.json({
-        name: genre.name,
-        description: genre.description,
+      return res.status(200).json({
+        name: genre?.name,
+        description: genre?.description,
       });
     } else {
-      res.status(404).json({ error: "Genre not found" });
+      return res.status(404).json({ error: "Genre not found" });
     }
   } catch (error) {
     console.error("Error fetching genre description:", error.message);
-    res.status(500).json({ error: "Failed to fetch genre description" });
+    return res.status(500).json({ error: "Failed to fetch genre description" });
   }
 });
 
